00196cb5b08ef27f4bf7598334569cb9e5259a1b
Peiki <peikimo@hotmail.it>
2017-01-31 18:46:47 +0100

    Added Static Enemy

---
+++ Assets/ReactiveTarget.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class ReactiveTarget	:MonoBehaviour{
+	public void ReactToHit(){
+		StartCoroutine(Die());
+	}
+	private IEnumerator Die(){
+		this.transform.Rotate(-75,0,0);
+		yield return new WaitForSeconds(1.5f);
+		Destroy(this.gameObject);
+	}
+}
