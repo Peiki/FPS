c322f84de8d355238170e155cbf33406ca08a7a7
Peiki <peikimo@hotmail.it>
2017-02-01 19:46:46 +0100

    Improved Ai (shooting)

---
+++ Assets/PlayerCharacter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class PlayerCharacter:MonoBehaviour{
+	private int _health;
+	void Start(){
+		_health=5;
+	}
+	public void Hurt(int damage){
+		_health-=damage;
+		Debug.Log("Health: "+ _health);
+	}	
+}
