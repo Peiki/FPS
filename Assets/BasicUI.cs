153d38bb960d4b9eae70cc4c19ff5e956182f28f
Peiki <peikimo@hotmail.it>
2017-02-15 20:09:00 +0100

    Added Canvas and UI Sprites

---
+++ Assets/BasicUI.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class BasicUI:MonoBehaviour{
+	void OnGUI(){
+		if(GUI.Button(new Rect(10,10,40,20),"Test"))
+			Debug.Log("Test button");
+	}
+}
